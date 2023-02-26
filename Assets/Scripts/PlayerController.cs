03d8f912447b2da886126b855c310f93e5501504
jchernin4 <jeremychernin@gmail.com>
2023-02-25 23:24:47 -0500

    Gabagoo!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -60,10 +60,10 @@ public class PlayerController : NetworkBehaviour {
                     if (s.isCorrect) {
                         score++;
 
-                        CmdPlayerScored(username.text);
+                        CmdPlayerScored(usernameText);
                         Debug.Log("Player scored");
                         if (score == winScore) {
-                            CmdPlayerWon(username.text);
+                            CmdPlayerWon(usernameText);
                         }
                     }
                 }
@@ -85,8 +85,6 @@ public class PlayerController : NetworkBehaviour {
 
     [ClientRpc(includeOwner = true)]
     private void PlayerScoredRpc(string name) {
-        if (!isLocalPlayer) return;
-        
         if (username.text.Equals(name)) {
             GameManager.instance.yourProgress.value = (score / (float)winScore);
             
