dab4e48746846024b0575ffeea83cd8a116731e6
Lucas Cleary <ljc5812@rit.edu>
2023-02-25 19:42:48 -0500

    math method

---
+++ Assets/Scripts/math.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class math {
+    private string equation(){
+        int choice = (int)Random.Range(1.0f,4.0f);
+        if(choice == 1){
+            int addx = (int)Random.Range(1.0f,50.0f);
+            int addy = (int)Random.Range(1.0f,49.0f);
+            answer = addx + addy;
+            return addx + " + " + addy + " = " + answer;
+
+        }
+        else if(choice == 2){
+            int subx = (int)Random.Range(1.0f,100.0f);
+            int suby = (int)Random.Range(1.0f,100.0f);
+            if(suby > subx){
+                answer = suby - subx;
+                return suby + " - " + subx + " = " + answer;
+            }
+            answer = subx - suby;
+            return subx + " - " + suby + " = " + answer;
+        }
+        else if(choice == 3){
+            int mulx = (int)Random.Range(0.0f,10.0f);
+            int muly = (int)Random.Range(1.0f,9.0f);
+            answer = mulx * muly;
+            return mulx + " + " + muly + " = " + answer;
+        }
+        else{
+            int divx = (int)Random.Range(0.0f, 10.0f);
+            int divy = (int)Random.Range(1.0f,9.0f);
+            int bignum = divx * divy;
+            return bignum + " / " + divx + " = " + divy;
+        }
+    }
+}
