f0e7fd066d2f2c625571e5d49f2764210559af53
Lucas Cleary <ljc5812@rit.edu>
2023-02-25 19:47:31 -0500

    math fix

--- Assets/Scripts/math.cs
+++ Assets/Scripts/math.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class math {
     private string equation(){
+        int answer = 0;
         int choice = (int)Random.Range(1.0f,4.0f);
         if(choice == 1){
             int addx = (int)Random.Range(1.0f,50.0f);
             int addy = (int)Random.Range(1.0f,49.0f);
-            int answer = addx + addy;
+            answer = addx + addy;
             return addx + " + " + addy + " = " + answer;
 
         }
@@ -16,16 +17,16 @@ public class math {
             int subx = (int)Random.Range(1.0f,100.0f);
             int suby = (int)Random.Range(1.0f,100.0f);
             if(suby > subx){
-                int answer = suby - subx;
+                answer = suby - subx;
                 return suby + " - " + subx + " = " + answer;
             }
-            int answer = subx - suby;
+            answer = subx - suby;
             return subx + " - " + suby + " = " + answer;
         }
         else if(choice == 3){
             int mulx = (int)Random.Range(0.0f,10.0f);
             int muly = (int)Random.Range(1.0f,9.0f);
-            int answer = mulx * muly;
+            answer = mulx * muly;
             return mulx + " + " + muly + " = " + answer;
         }
         else{
